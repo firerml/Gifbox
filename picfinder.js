d42745f28cf492f48fc4b9bc7b4e38a015210062
mike <firerml@gmail.com>
2016-04-09 16:03:18 -0400

    Can right click images to save them to sync storage

---
+++ picfinder.js
@@ -0,0 +1,35 @@
+var bkg = chrome.extension.getBackgroundPage();
+var syncStorage = chrome.storage.sync
+
+function savePic(info, tab) {
+	var promptedName = prompt('Name this picture');
+	promptedName = promptedName.trim()
+	// TODO: stop here if promptedName is null.
+	syncStorage.get(null, function(results) {
+		var highestId = 0
+		for (itemId in results) {
+			if (itemId > highestId) {
+				highestId = itemId;
+			}
+		}
+
+		var newItemId = Number(highestId) + 1
+		var saveObj = {}
+		saveObj[newItemId] = {
+			'name': promptedName,
+			'imageUrl': info.srcUrl,
+			'tags': []
+		}
+
+		syncStorage.set(saveObj, function() {
+			// TODO: If runtime.lastError, alert user of error.
+			return
+		});	
+	});
+};
+
+chrome.contextMenus.create({
+	title: 'Save Sweet Pic, Bro!',
+	contexts: ['image'],
+	onclick: savePic
+});
